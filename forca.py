bf12568693bbb495886114e781eeb2cbce4ed967
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2021-11-12 00:36:38 -0300

    Forca revelando letras acertadas

--- forca.py
+++ forca.py
@@ -9,6 +9,41 @@ def jogar():
 
     palavra_secreta = sorteiar_palavra(tema)
 
+    letras_descobertas = []
+    for letra in palavra_secreta:
+        letras_descobertas.append('_')
+
+    for index in range(len(letras_descobertas)):
+        print (letras_descobertas[index], end = ' ')
+    
+    print('\n')
+    
+    chute = input("Chute uma letra! ").lower()
+    if (len(chute) > 1):
+        input("Chute apenas uma letra! ").lower()
+
+    posicoes_descobertas = []
+    posicao = 0
+    
+    for letra in palavra_secreta:
+        if (chute == letra):
+            posicoes_descobertas.append(posicao)
+        posicao += 1
+    
+
+    for index in range(len(posicoes_descobertas)):
+        letra_acertada = posicoes_descobertas[index]
+
+        letras_descobertas[letra_acertada] = palavra_secreta[letra_acertada]
+
+    for index in range(len(letras_descobertas)):
+        print (letras_descobertas[index], end = ' ')
+        
+    
+
+
+    
+
 def cabecalho():
     print("\n///////////////////////////////////////////")
     print("/// Jogo da forca - Adivinhe a palavra! ///")
